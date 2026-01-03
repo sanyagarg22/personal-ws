5a3608ac2adf6c3bfb32c01d8410dba52a11aa06
Sanya Garg <sanya.garg@gmail.com>
2026-01-02 23:36:13 -0800

    fixed error

--- ws/app/components/paint/Projects.tsx
+++ ws/app/components/paint/Projects.tsx
@@ -65,7 +65,7 @@ function ProjectItem({ title, description, tags, githubUrl, liveUrl, year, index
                   rel="noopener noreferrer"
                   className="text-gray-600 hover:text-[#7092be] transition-colors underline text-sm"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                   </svg>
                 </a>
